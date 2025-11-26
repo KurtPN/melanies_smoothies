920d18393dbf0ae33a9658a83681eee20b1a2e78
KurtPN <kurtgnielsen@gmail.com>
2025-11-26 12:29:58 +0000

    Update streamlit_app.py
    Added Search_on variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the Snowpark Dataframe to Pandas Dataaframe so we can use the LOC fucntion
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop
+#st.dataframe(pd_df)
+#st.stop
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: '
@@ -39,6 +39,11 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.stop
+      
         #Return nutrition information via API
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("http://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
