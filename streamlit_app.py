14aa9cc986bca151483cda57f355914fa6350c98
KurtPN <kurtgnielsen@gmail.com>
2025-11-26 12:33:28 +0000

    Update streamlit_app.py
    Removed st.stop after search_on definition

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        st.stop
+        st.write('The search value for ', FRUIT_CHOSEN,' is ', SEARCH_ON, '.')
       
         #Return nutrition information via API
         st.subheader(fruit_chosen + ' Nutrition Information')
